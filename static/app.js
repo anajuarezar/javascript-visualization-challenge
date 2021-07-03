c5a1c96f16c4bb97cb38990a7d8ed7035419e461
anajuarezar <ana.juarezar@gmail.com>
2021-07-03 15:10:09 -0700

    Default plot

--- static/app.js
+++ static/app.js
@@ -1,10 +1,40 @@
 d3.json("./samples.json").then(function(data) {
-    var sample_values = data.samples[0].sample_values;
-    var otu_ids = data.samples[0].otu_ids;
-    var otu_labels = data.samples[0].otu_labels;
-    console.log(sample_values);
+console.log(data);
 });
 
 // We begin to do the default plot
+function unpack(rows, index) {
+    return rows.map(function(row) {
+      return row[index];
+    });
+}
 
+function firstPlot() {
+    var sample_values = data.samples[0].sample_values;
+    var sampleSorted = sample_values.reverse();
+    var slicedSample = sampleSorted.slice(0,10);
+    console.log(slicedSample);
+    var otu_ids = slicedSample.map(object => object.otu_ids);
+    var otu_labels = slicedSample.map(object => object.otu_labels);
 
+    var trace1 = {
+        x: slicedSample,
+        y: otu_ids,
+        text: otu_labels,
+        type: "bar",
+        orientation: "h"
+      };
+      
+      // data
+      var data = [trace1];
+      
+      // Apply the group bar mode to the layout
+      var layout = {
+        title: "Top 10 Bacteria Cultures Found"
+      };
+
+      Plotly.newPlot("bar", data, layout);
+
+}
+
+firstPlot()
