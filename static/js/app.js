83f61d080c791eaf99b6f709d5f47b8e3a7f2721
anajuarezar <ana.juarezar@gmail.com>
2021-07-03 18:32:28 -0700

    change app to js

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,46 @@
-d3.json("/samples.json").then(function(data) {
+d3.json("./samples.json").then((data) => {
     console.log(data);
-    var sample_values = data.samples[0].sample_values;
-    var otu_ids = data.samples[0].otu_ids;
-    var otu_labels = data.samples[0].otu_labels;
 });
+
+// We begin to do the default plot
+function unpack(rows, index) {
+    return rows.map(function(row) {
+      return row[index];
+    });
+}
+
+function firstPlot() {
+
+    var sampleValues = data.samples[0].sample_values;
+    var otuID = data.samples[0].otu_ids; 
+    var otuLabels = data.samples[0].otu_labels;
+    console.log(sampleValues);
+
+    // Time to slice and sort!
+
+    var slicedSample = sampleValues.slice(0, 10).reverse();
+    var slicedotuID = otuID.slice(0, 10).reverse();
+    var toplabels = slicedotuID.map((each => "OTU" + otu_labels));
+    console.log(slicedSample);
+
+    var trace1 = {
+        x: slicedSample,
+        y: slicedotuID,
+        text: toplabels,
+        type: "bar",
+        orientation: "h"
+      };
+      
+      // data
+      var data = [trace1];
+      
+      // Apply the group bar mode to the layout
+      var layout = {
+        title: "Top 10 Bacteria Cultures Found"
+      };
+
+      Plotly.newPlot("bar", data, layout);
+
+}
+
+firstPlot();
